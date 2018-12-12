521b98b85d9cfd2448235add088420b47121eb3c
yitepeli <yitepeli@gmail.com>
2018-12-12 21:56:10 +0300

    first

--- evalData.py
+++ evalData.py
@@ -10,68 +10,81 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn import preprocessing
 import itertools
+import pandas as pd
 
-def main():
 
-    data = np.array([])
-    label = np.array([])
+def processData():
+    outData = np.array([])
+    outLabel = np.array([])
+
+    #get data from PPData.csv
     with open('PPData.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
+
+        #For each row in file
         for row in spamreader:
-            #print(row[6])
-            labelVal=0
-            data2Val=row[2]
-            if row[6]=="LOF":
-                labelVal=1
-            if row[2]=="*":
-                data2Val="X"
-            if len(label)==0:
-                data = np.array([[row[1],data2Val]])
-                label = np.array([labelVal])                
+            labelVal = 0
+            data2Val = row[2]
+
+            #If LOF then label=1, else label = 0
+            if row[6] == "LOF":
+                labelVal = 1
+
+            #If new aa is * then it is converted to X
+            if row[2] == "*":
+                data2Val = "X"
+            if len(outLabel) == 0:
+                outData = np.array([[row[1], data2Val]])
+                outLabel = np.array([labelVal])
             else:
-                data = np.append(data, [[row[1],data2Val]],axis = 0) 
-                label = np.append(label, [labelVal]) 
-    #print(data)
-    #print(data[:,0])
-    c = itertools.chain(data[:,0], data[:,1])
-    anyFeature = list(set(list(c)))
-    #d = data[:,0]+data[:,1]
-    #anyFeature = list(set(data[:,0]+data[:,1]))
-    #print(anyFeature)
-    #list(le.classes_)
+                outData = np.append(outData, [[row[1], data2Val]], axis=0)
+                outLabel = np.append(outLabel, [labelVal])
 
-    enc = preprocessing.LabelEncoder()
-    enc.fit(anyFeature)  
-    newFeatures = enc.transform(anyFeature)
-    print(anyFeature)
-    print(list(newFeatures))
-    print(data)
-    #print(list(enc.classes_))
-    encData = np.array([])
-    for rowNo in range(len(data)):
-        ind1 = anyFeature.index(data[rowNo][0])
-        ind2 = anyFeature.index(data[rowNo][1])
-        if len(encData)==0:
-            encData = np.array([[newFeatures[ind1],newFeatures[ind2]]])      
-        else:
-            encData = np.append(encData, [[newFeatures[ind1],newFeatures[ind2]]],axis = 0) 
-    #enc.transform(data)
-    
-    print(encData)
-    
+    return outData, outLabel
+
+def predict():
+    data, labels = processData()
+
+    # Encode amino acids
+    df = pd.DataFrame({'A': data[:, 0].tolist(), 'B': data[:, 1].tolist()})
+    OneHotData = np.array(pd.get_dummies(df, prefix=['aa0', 'aa1']).values.tolist())
+    # print(OneHotData)
+
+    # Train and Test Data 80%-20%
+    cutterIndex = round(8 * len(labels) / 10)
+    oneHotDataTrain = OneHotData[:cutterIndex]
+    oneHotDataTest = OneHotData[cutterIndex:]
+    labelsTrain = labels[:cutterIndex]
+    labelsTest = labels[cutterIndex:]
+    sizeOfTrain = len(labelsTrain)
+    sizeOfTest = len(labelsTest)
+
+    # Calculate Base Accuracy
+    countOfLoss = 0
+    for row in labelsTest:
+        if row == 1:
+            countOfLoss += 1
+    baseAcc = max(countOfLoss, sizeOfTest - countOfLoss) / sizeOfTest
+    print("Base Accuracy: " + str(baseAcc))
+
+    # Create SVM Model
     clf = SVC(gamma='auto')
-    clf.fit(encData, label) 
-    #print(list(enc.inverse_transform([1,9])))
-    count=0
-    '''for i in range(len(label)):
-        if clf.predict([encData[i]]) == [1]:
-           count += 1
-    print(len(label))'''
-    print(data[0])
-    print(encData[0])
-    print(clf.predict([encData[0]]))
-    #preprocessing.OneHotEncoder()
+    clf.fit(oneHotDataTrain, labelsTrain)
 
+    # Predict
+    testPredictions = clf.predict(oneHotDataTest)
+
+    # Prediction Statistics
+    countOfCorrects = 0
+    for i in range(len(labelsTest)):
+        if testPredictions[i] == labelsTest[i]:
+            countOfCorrects += 1
+
+    predictionAcc = countOfCorrects / sizeOfTest
+    print("Prediction Accuracy: " + str(predictionAcc))
+
+def main():
+    predict()
     
 if __name__== "__main__":
     main()
